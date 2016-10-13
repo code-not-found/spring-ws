0862698f8f0c9eb6b5c9f89cdd89609cb5b2d300
codenotfound.com <codenotfound.com@gmail.com>
2016-10-13 06:09:17 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# spring-ws
-spring-ws projects
+spring-ws
+============
+
+This repository contains the source code for the spring-ws examples posted on http://www.codenotfound.com
+
+In case of questions or remarks please leave a comment in the respective blog post or send a mail to codenotfound.com@gmail.com. Thanks!
---
+++ springws-helloworld-example/src/test/java/com/codenotfound/SpringWsApplicationTests.java
@@ -0,0 +1,26 @@
+package com.codenotfound;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.codenotfound.client.HelloWorldClient;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+public class SpringWsApplicationTests {
+
+    @Autowired
+    private HelloWorldClient helloWorldClient;
+
+    @Test
+    public void testSayHello() {
+        assertThat(helloWorldClient.sayHello("John", "Doe"))
+                .isEqualTo("Hello John Doe!");
+    }
+}
