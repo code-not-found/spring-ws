1e4a7d80610a0281ca2c86c5173167269a3e8ac4
codenotfound.com <codenotfound.com@gmail.com>
2017-03-10 05:47:56 +0100

    Adding spring-ws-integration-test

---
+++ spring-ws-integration-test/src/main/java/com/codenotfound/order/OrderApplication.java
@@ -0,0 +1,12 @@
+package com.codenotfound.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+}
