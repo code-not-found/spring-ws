07adda5e6ac3297d5d50875e7c479029307a4f21
codenotfound <codenotfound.com@gmail.com>
2017-12-08 16:12:33 +0100

    add spring-ws-timeout

---
+++ spring-ws-timeout/README.md
@@ -0,0 +1,5 @@
+# spring-ws-timeout
+
+[![Quality Gate](https://sonarqube.com/api/badges/gate?key=com.codenotfound:spring-ws-timeout)](https://sonarqube.com/dashboard/index/com.codenotfound:spring-ws-timeout)
+
+A sample project that illustrates how to configure a client timeout using the `HttpUrlConnectionMessageSender` implementation.
---
+++ spring-ws-timeout/src/main/java/com/codenotfound/ws/client/ClientConfig.java
@@ -0,0 +1,42 @@
+package com.codenotfound.ws.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.ws.client.core.WebServiceTemplate;
+
+@Configuration
+public class ClientConfig {
+
+  @Value("${client.timeout}")
+  private int timeout;
+
+  @Bean
+  Jaxb2Marshaller jaxb2Marshaller() {
+    Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
+    jaxb2Marshaller.setContextPath("org.example.ticketagent");
+
+    return jaxb2Marshaller;
+  }
+
+  @Bean
+  public WebServiceTemplate webServiceTemplate() {
+    WebServiceTemplate webServiceTemplate = new WebServiceTemplate();
+    webServiceTemplate.setMarshaller(jaxb2Marshaller());
+    webServiceTemplate.setUnmarshaller(jaxb2Marshaller());
+    webServiceTemplate.setDefaultUri("http://localhost:9090/codenotfound/ws/ticketagent");
+    webServiceTemplate.setMessageSender(httpUrlConnectionMessageSenderTimeout());
+
+    return webServiceTemplate;
+  }
+
+  @Bean
+  public HttpUrlConnectionMessageSenderTimeout httpUrlConnectionMessageSenderTimeout() {
+    HttpUrlConnectionMessageSenderTimeout httpUrlConnectionMessageSenderTimeout =
+        new HttpUrlConnectionMessageSenderTimeout();
+    httpUrlConnectionMessageSenderTimeout.setTimeout(timeout);
+
+    return httpUrlConnectionMessageSenderTimeout;
+  }
+}
